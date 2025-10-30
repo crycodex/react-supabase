1b6e525e4f0941d15c167ac7d84b3acf46382582
crycodex <recaldecd@gmail.com>
2025-10-30 09:37:21 -0500

    Remove React logo from App component, pass supabase instance to App component in main entry file, and delete unused react.svg asset.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { supabase } from './supabase/client.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App supabase={supabase} />
   </StrictMode>,
 )
